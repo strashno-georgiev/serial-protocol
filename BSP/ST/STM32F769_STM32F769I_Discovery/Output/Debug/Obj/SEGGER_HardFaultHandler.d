fea37591f47375fc3fdab77e8da9ec8888f209ff
Kaloyan Georgiev <kaloyan.georgieev@gmail.com>
2023-02-01 23:32:15 +0200

    Remove object and project meta files from repo

--- BSP/ST/STM32F769_STM32F769I_Discovery/Output/Debug/Obj/SEGGER_HardFaultHandler.d
+++
@@ -1,2 +0,0 @@
-/home/kaloyangeorgiev/Downloads/SeggerEval/SeggerEval_STM32F769_ST_STM32F769I_Discovery_CortexM_SES_220712/BSP/ST/STM32F769_STM32F769I_Discovery/Output/Debug/Obj/SEGGER_HardFaultHandler.o: \
-  /home/kaloyangeorgiev/Downloads/SeggerEval/SeggerEval_STM32F769_ST_STM32F769I_Discovery_CortexM_SES_220712/SEGGER/SEGGER_HardFaultHandler.c
--- BSP/ST/STM32F769_STM32F769I_Discovery/Output/Debug/Obj/STM32F7xx_Startup.d
+++
@@ -1,2 +0,0 @@
-/home/kaloyangeorgiev/Downloads/SeggerEval/SeggerEval_STM32F769_ST_STM32F769I_Discovery_CortexM_SES_220712/BSP/ST/STM32F769_STM32F769I_Discovery/Output/Debug/Obj/STM32F7xx_Startup.o: \
- /home/kaloyangeorgiev/Downloads/SeggerEval/SeggerEval_STM32F769_ST_STM32F769I_Discovery_CortexM_SES_220712/BSP/ST/STM32F769_STM32F769I_Discovery/Setup/DeviceSupport/STM32F7xx_Startup.s
